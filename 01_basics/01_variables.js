b666c9b3bfb8f05b9a8c9837ccd44a50ff404fb9
Dhruv Agrawal <119067939+dhruvaa24@users.noreply.github.com>
2024-12-26 20:19:47 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 148984
+let accountEmail = "abc@gmail.com"
+var accountPassword = "12345"
+accountCity = "Ayodhya"
+let accountState;
+
+//accountId = 2 // not allowed
+
+accountEmail = "asd@asd.com"
+accountPassword = "13579"
+accountCity = "Delhi"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version 
+
+// alert(3+3)  //we are using nodejs not browser
+
+// console.log(3+3) 
+// console.log("dhruv") //use new lines to ensure readabiity
+
+let name = "dhruv"
+let age = 20
+let isLoggedIn = false
+
+// number 
+// bigInt
+// string => use double qoutes
+// boolean => true/false
+// null=> representation of empty value
+// undefined => value not assigned
+// symbol
+// object 
+
+console.table([typeof name, typeof age, typeof isLoggedIn, typeof null, typeof undefined])
+
+// datatype of null is object and of undefined is undefined only
