ba307f10e8aa347a4c18eec807606bfddf2a6e56
Dhruv Agrawal <119067939+dhruvaa24@users.noreply.github.com>
2024-12-27 18:51:37 +0000

    operations updated

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,12 +1,12 @@
-// let score = undefined
+/*let score = undefined
 
-// console.log(typeof score)
-// console.log(typeof(score))
+console.log(typeof score)
+console.log(typeof(score))
 
-// let valueInNumber = Number(score)
-// console.log(typeof valueInNumber)
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
 
-// console.log(valueInNumber)
+console.log(valueInNumber) */
 
 // Number conversion of different data types -
 // "33" => 33
@@ -15,19 +15,43 @@
 // null => 0
 // undefined => NaN
 
-let isLoggedIn = "hi"
+/* let isLoggedIn = "hi"
 
 let boolIsLoggedIn = Boolean(isLoggedIn)
-console.log(boolIsLoggedIn)
+console.log(boolIsLoggedIn) */
 
 // Boolean conversion - 
 // 0 => false; 1 => true;
 // "" => false;
 // "hi" => true;
 
-let someNumber = 33
+/* let someNumber = 33
 
 let stringNumber = String(someNumber)
 
 console.log(stringNumber)
-console.log(typeof stringNumber)
+console.log(typeof stringNumber) */
+
+// **************Operations************** //
+
+/* let value = 3
+let negValue = -value
+console.log(negValue)
+
+let str1 = "hello"
+let str2 = " dhruv"
+let str3 = str1+str2
+console.log(str3)
+
+console.log("1"+2);
+console.log("1"+"2");
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2"); */
+
+console.log(+true)
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2 //not preferred
+
+
