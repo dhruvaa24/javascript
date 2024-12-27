24af7394e222adc2254618ed1ca4260ef07cbbf2
Dhruv Agrawal <119067939+dhruvaa24@users.noreply.github.com>
2024-12-27 15:53:01 +0000

    Conversion of data types

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,5 +1,33 @@
-let score = "33"
+// let score = undefined
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+
+// console.log(valueInNumber)
+
+// Number conversion of different data types -
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = "hi"
+
+let boolIsLoggedIn = Boolean(isLoggedIn)
+console.log(boolIsLoggedIn)
+
+// Boolean conversion - 
+// 0 => false; 1 => true;
+// "" => false;
+// "hi" => true;
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
