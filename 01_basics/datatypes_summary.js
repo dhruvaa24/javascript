083e136387ef2b5923542e0d03686a9b4e9909b0
Dhruv Agrawal <119067939+dhruvaa24@users.noreply.github.com>
2024-12-28 10:27:58 +0000

    datatype summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,34 @@
+// Primitive
+// 7 types : String, Number, Boolean, BigInt, null, undefined, Symbol
+
+/* const score = 100;
+console.log(typeof score);
+
+const id = Symbol('123')
+const id2 = Symbol('123')
+console.log(id == id2);
+
+const bigNumber = 11231213435654675n
+console.log(typeof bigNumber); */
+
+
+// Refrence type or Non primitive
+// Arrays, Objects, Functions
+
+const heroes = ["ironman", "batman", "hulk", "thor"];
+
+let myObj = {
+    name: "Dhruv",
+    age: 22,
+    email: "dhruvagrawal247@gmail.com"
+}
+
+const myFunction = function (){
+    console.log("Hello World !");    
+}
+
+console.log(typeof null); // return object as it is unique
+console.log(typeof heroes);
+console.log(typeof myObj);
+console.log(typeof myFunction); // it returns function that means it is a function object
+
