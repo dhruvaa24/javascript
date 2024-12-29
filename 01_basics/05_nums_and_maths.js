22592946142c050df423dfd87838ad580be54212
Dhruv Agrawal <119067939+dhruvaa24@users.noreply.github.com>
2024-12-29 17:47:44 +0000

    Numbers in maths

---
+++ 01_basics/05_nums_and_maths.js
@@ -0,0 +1,30 @@
+/* const score = 400
+console.log(score);
+
+const balance = new Number(40000)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.8973
+console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString()); */
+
+/***********Math**********/
+/* console.log(Math.abs(-4));
+console.log(Math.round(4.2));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.2));
+console.log(Math.sqrt(4.2));
+console.log(Math.pow(4.2,2));
+console.log(Math.min(4,2,6,8,9));
+console.log(Math.max(4,2,6,8,9)); */
+console.log(Math.floor((Math.random()*6)+1));
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor((Math.random() * (max-min+1)) + min));
