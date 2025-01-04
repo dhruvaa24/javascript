cc54d9975181f4f57b6cfdcceae10da63d3e41b9
Dhruv Agrawal <119067939+dhruvaa24@users.noreply.github.com>
2025-01-04 21:06:38 +0000

    global and local scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,14 @@
+var c = 300
+let a = 300
+
+if(true){
+    let a = 10
+    const b = 20
+    var c = 30 
+    console.log("Inner: ", a);
+       
+}
+
+console.log("Outer: ", a);
+// console.log(b);
+console.log(c); // var shouldnt be used as it always has global scope
