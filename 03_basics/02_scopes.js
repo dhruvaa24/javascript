5fe3ebe165b374b836bf296a83b1f4750f02cd10
Dhruv Agrawal <119067939+dhruvaa24@users.noreply.github.com>
2025-01-05 19:17:47 +0000

    scope level and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,10 +5,45 @@ if(true){
     let a = 10
     const b = 20
     var c = 30 
-    console.log("Inner: ", a);
+    // console.log("Inner: ", a);
        
 }
 
-console.log("Outer: ", a);
+// console.log("Outer: ", a);
 // console.log(b);
-console.log(c); // var shouldnt be used as it always has global scope
+// console.log(c); // var shouldnt be used as it always has global scope
+
+
+function one(){
+    const username = "dhruv"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);        
+    }
+    // console.log(website); //will give error as website is not in its scope
+    two()
+}
+// one()
+
+if(true){
+    const username = "dhruv"
+    if(username === "dhruv"){
+        const website = " youtube"
+        console.log(username + website);
+    }
+    // console.log(website);  // error 
+}
+// console.log(username); // error
+
+/*************interesting*************/
+console.log(addOne(5));
+function addOne(val){ // this is a basic function
+    return val+1;
+}
+
+console.log(addTwo(5)); // this will give error but above method wont when accessing before the function declarartion itself
+const addTwo = function(num){ // this can be called a expression
+    return num+2
+}
+
