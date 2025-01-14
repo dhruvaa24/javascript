9bff49622649d38d5d2c33bb65750672ef305780
Dhruv Agrawal <119067939+dhruvaa24@users.noreply.github.com>
2025-01-14 14:51:06 +0000

    Basic Project 1

--- 07_projects/01_project1/script.js
+++ 07_projects/01_project1/script.js
@@ -1,22 +1,35 @@
-const buttons = document.querySelectorAll('.button')
-const body = document.querySelector("body")
+// my simpler version of main code 
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+console.log(buttons);
 
-buttons.forEach( function(button){
-    console.log(button);
-    button.addEventListener('click', function(e){
-        console.log(e);
+buttons.forEach(function(button){
+    button.addEventListener('click',function(e){
         console.log(e.target);
-        if(e.target.id === 'grey'){
-            body.style.backgroundColor = e.target.id
-        }
-        if(e.target.id === 'white'){
-            body.style.backgroundColor = e.target.id
-        }
-        if(e.target.id === 'blue'){
-            body.style.backgroundColor = e.target.id
-        }
-        if(e.target.id === 'yellow'){
-            body.style.backgroundColor = e.target.id
-        }
+        body.style.backgroundColor = e.target.id;   
     })
 })
+
+
+// code given
+// const buttons = document.querySelectorAll('.button');
+// const body = document.querySelector('body');
+
+// buttons.forEach(function(button){
+//     button.addEventListener('click',function(e){
+//         console.log(e);
+//         console.log(e.target);
+//         if(e.target.id === 'grey'){
+//             body.style.backgroundColor = e.target.id;
+//         }
+//         if(e.target.id === 'white'){
+//             body.style.backgroundColor = e.target.id;
+//         }
+//         if(e.target.id === 'blue'){
+//             body.style.backgroundColor = e.target.id;
+//         }
+//         if(e.target.id === 'yellow'){
+//             body.style.backgroundColor = e.target.id;
+//         }
+//     })
+// })
